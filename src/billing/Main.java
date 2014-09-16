d3879593d0425cae4c20afd21032ce644fc0ab00
vsomov <zemnovodnoe@gmail.com>
2014-09-16 12:23:09 +0300

    connectors, prop file

--- src/billing/Main.java
+++ src/billing/Main.java
@@ -5,41 +5,69 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 //import com.mysql.jdbc.Driver;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		
+		Properties prop = new Properties();
+    	InputStream input = null;
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
+		dbconnect dbc = new dbconnect();
 		System.out.println("Accessing database...");
+		
 		try {
-//			new com.mysql.jdbc.Driver();
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-// conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/billing?user=root&password=");
-			
-// move connectors to the separate class, compile .jar from the command line, create .bat tool for the users. 			
-			String connectionUrl = "jdbc:mysql://localhost:3306/billing";
-			String connectionUser = "root";
-			String connectionPassword = "";
-			conn = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("SELECT * FROM users");
-			while (rs.next()) {
-				String id = rs.getString("id");
-				String name = rs.getString("name");
-				String balance = rs.getString("balance");
-				System.out.println("ID: " + id + ", First Name: " + name
-						+ ", Last Name: " + balance);
+			 
+    		String filename = "config.properties";
+    		input = Main.class.getClassLoader().getResourceAsStream(filename);
+    		if(input==null){
+    	            System.out.println("Sorry, unable to find " + filename);
+    		    return;
+    		}
+ 
+    		//load a properties file from class path, inside static method
+    		prop.load(input);
+ 
+                //get the property value and print it out
+                System.out.println(prop.getProperty("database"));
+    	        System.out.println(prop.getProperty("dbuser"));
+    	        System.out.println("Password " + (prop.getProperty("dbpassword")));
+    	        
+    	        conn = dbc.run(prop.getProperty("database"), prop.getProperty("dbuser"), prop.getProperty("dbpassword"));
+				stmt = conn.createStatement();
+				rs = stmt.executeQuery("SELECT * FROM users");
+				while (rs.next()) {
+					String id = rs.getString("id");
+					String name = rs.getString("name");
+					String balance = rs.getString("balance");
+					System.out.println("ID: " + id + ", First Name: " + name
+							+ ", Last Name: " + balance);
+				}
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+        } finally{
+        	if(input!=null){
+        		try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
-			try { if (stmt != null) stmt.close(); } catch (SQLException e) { e.printStackTrace(); }
-			try { if (conn != null) conn.close(); } catch (SQLException e) { e.printStackTrace(); }
-		}
-		System.out.println("Done reading from the database.");
+        		
+				
+				System.out.println("Done reading from the database.");
+				
+				
+				
+			
+        	}
+        }
+		
+		
 	}
 }
---
+++ src/billing/dbconnect.java
@@ -0,0 +1,29 @@
+package billing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class dbconnect {
+	public Connection run(String database, String dbuser, String dbpassword){
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+//			new com.mysql.jdbc.Driver();
+			
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+// conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/billing?user=root&password=");
+			
+// move connectors to the separate class, compile .jar from the command line, create .bat tool for the users. 			
+//			String database = "jdbc:mysql://localhost:3306/billing";
+			conn = DriverManager.getConnection(database, dbuser, dbpassword);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
